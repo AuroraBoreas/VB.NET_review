634f22ae3ccd2f051cd6652d15c6d735b5397659
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-06-21 22:45:26 +0800

    Add files via upload

---
+++ VB2015_in_24Hours_code_samples_001/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button_Select = New System.Windows.Forms.Button()
+        Me.Button_Close = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button_ChangeFont = New System.Windows.Forms.Button()
+        Me.TextBox_ShowFontSize = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(515, 418)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button_Select
+        '
+        Me.Button_Select.Location = New System.Drawing.Point(559, 23)
+        Me.Button_Select.Name = "Button_Select"
+        Me.Button_Select.Size = New System.Drawing.Size(125, 23)
+        Me.Button_Select.TabIndex = 1
+        Me.Button_Select.Text = "Select"
+        Me.Button_Select.UseVisualStyleBackColor = True
+        '
+        'Button_Close
+        '
+        Me.Button_Close.Location = New System.Drawing.Point(559, 63)
+        Me.Button_Close.Name = "Button_Close"
+        Me.Button_Close.Size = New System.Drawing.Size(125, 23)
+        Me.Button_Close.TabIndex = 2
+        Me.Button_Close.Text = "Close"
+        Me.Button_Close.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button_ChangeFont
+        '
+        Me.Button_ChangeFont.Location = New System.Drawing.Point(559, 113)
+        Me.Button_ChangeFont.Name = "Button_ChangeFont"
+        Me.Button_ChangeFont.Size = New System.Drawing.Size(125, 23)
+        Me.Button_ChangeFont.TabIndex = 3
+        Me.Button_ChangeFont.Text = "ChangeFont"
+        Me.Button_ChangeFont.UseVisualStyleBackColor = True
+        '
+        'TextBox_ShowFontSize
+        '
+        Me.TextBox_ShowFontSize.Location = New System.Drawing.Point(559, 162)
+        Me.TextBox_ShowFontSize.Multiline = True
+        Me.TextBox_ShowFontSize.Name = "TextBox_ShowFontSize"
+        Me.TextBox_ShowFontSize.Size = New System.Drawing.Size(125, 42)
+        Me.TextBox_ShowFontSize.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(696, 450)
+        Me.Controls.Add(Me.TextBox_ShowFontSize)
+        Me.Controls.Add(Me.Button_ChangeFont)
+        Me.Controls.Add(Me.Button_Close)
+        Me.Controls.Add(Me.Button_Select)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button_Select As Button
+    Friend WithEvents Button_Close As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button_ChangeFont As Button
+    Friend WithEvents TextBox_ShowFontSize As TextBox
+End Class
---
+++ VB2015_in_24Hours_code_samples_001/Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+
+    Private Sub Button_Select_Click(sender As Object, e As EventArgs) Handles Button_Select.Click
+        'change form's font in runtime. retrieve this form's FontFamily, then change font size via Font class
+        Me.Font = New Font(Me.Font.FontFamily, 14, GraphicsUnit.Point)
+        Dim BaseDir As String = Application.StartupPath
+
+        With Me.OpenFileDialog1
+            .Title = "Select a picture"
+            .RestoreDirectory = True
+            .Filter = "Windows Bitmaps}|*.jpg;*.png;*.gif"
+            .CheckFileExists = True
+
+            '.ShowDialog w/o () works as well
+            If .ShowDialog() = DialogResult.OK Then
+                'app title changes in runtime
+                Me.Text = String.Format("Picture Viewer({0})", .FileName)
+                'app icon changes in runtime
+                Dim icon As New System.Drawing.Icon(String.Format("{0}\{1}", BaseDir, "robot.ico"))
+                Me.Icon = icon
+                'break image stream
+                icon.Dispose()
+                'picturebox1 backcolor changes in runtime
+                Me.PictureBox1.BackColor = Color.FromArgb(25, 255, 125)
+                'load picture
+                Me.PictureBox1.Image = Image.FromFile(.FileName)
+                'Me.PictureBox1.Image.Dispose()
+            End If
+        End With
+
+        Dim rnd As New Random
+        Dim r, g, b As Integer
+
+        'get random r, g, b between 0, 255
+        r = rnd.Next(0, 255)
+        g = rnd.Next(0, 255)
+        b = rnd.Next(0, 255)
+        'change form's background color in runtime
+        Me.BackColor = Color.FromArgb(r, g, b)
+    End Sub
+
+    Private Sub Button_Close_Click(sender As Object, e As EventArgs) Handles Button_Close.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button_ChangeFont_Click(sender As Object, e As EventArgs) Handles Button_ChangeFont.Click
+
+        'get a random number btw 1 and 100
+        Dim FontSize As Integer, r As Integer, g As Integer, b As Integer
+        '.NET Random class must be initialized to get a new instance or it may replicate previous result 
+        Dim rnd As New Random
+        'get random font size between 8, 20
+        FontSize = rnd.Next(8, 24)
+        'get random r, g, b between 0, 255
+        r = rnd.Next(0, 255)
+        g = rnd.Next(0, 255)
+        b = rnd.Next(0, 255)
+        'change form's FontFamily to *Tahoma*, and change font size dynamically
+        Me.Font = New Font("Tahoma", FontSize, GraphicsUnit.Point)
+        'change textbox's background color in runtime
+        With Me.TextBox_ShowFontSize
+            .Text = Me.Font.FontFamily.Name & "," & FontSize
+            .BorderStyle = BorderStyle.None
+            .BackColor = Color.FromArgb(r, g, b)
+        End With
+
+
+    End Sub
+End Class
