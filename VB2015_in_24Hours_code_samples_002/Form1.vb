725ff194c82248c24ab1d37a3f02595e689e7139
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-06-22 23:03:49 +0800

    Add files via upload

---
+++ VB2015_in_24Hours_code_samples_002/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim objGraphic As Graphics
+        objGraphic = Me.CreateGraphics
+        objGraphic.Clear(SystemColors.Control)
+        Dim rnd_pen As Pen
+        Dim rnd As New Random
+        rnd_pen = Choose(rnd.Next(1, 11),
+                         Pens.Black,
+                         Pens.Cyan,
+                         Pens.Red,
+                         Pens.Blue,
+                         Pens.AliceBlue,
+                         Pens.Crimson,
+                         Pens.Green,
+                         Pens.Silver,
+                         Pens.DarkKhaki,
+                         Pens.Azure,
+                         Pens.BlueViolet
+                    )
+
+        objGraphic.DrawRectangle(rnd_pen,
+                                 Me.PictureBox1.Left - 1,
+                                 Me.PictureBox1.Top - 1,
+                                 Me.PictureBox1.Width + 1,
+                                 Me.PictureBox1.Height + 1)
+        objGraphic.Dispose()
+
+    End Sub
+End Class
