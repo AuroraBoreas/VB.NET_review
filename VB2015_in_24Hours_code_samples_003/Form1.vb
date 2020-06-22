8ada8418b4819454036372fc3638749cb6642413
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-06-22 23:07:33 +0800

    Add files via upload

---
+++ VB2015_in_24Hours_code_samples_003/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Font = New Font("Tahoma", 11)
+        Dim intIndex As Int16
+        Dim strText As String = vbNullString
+
+        For intIndex = 0 To Me.Controls.Count - 1
+            strText &= String.Format("control #{0}, name is {1} " & vbCrLf, intIndex, Me.Controls(intIndex))
+        Next
+        Me.TextBox1.Text = strText
+    End Sub
+
+End Class
