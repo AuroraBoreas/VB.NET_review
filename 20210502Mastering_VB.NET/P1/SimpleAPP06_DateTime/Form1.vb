46189443f9c9b3a08bd7039783e0cbe1e78eb0ba
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-05-09 15:22:04 +0800

    Add files via upload

---
+++ 20210502Mastering_VB.NET/P1/SimpleAPP06_DateTime/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With Me
+            .Label1.AutoSize = True
+            .Timer1.Interval = 1_000
+            .Timer1.Start()
+            .Text = "SimpleAPP06, v0.01 @ZL, 20210509"
+        End With
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        With Me
+            .Label1.Text = Date.Now.ToString("yyyy-MMM-dd hh:mm:ss")
+        End With
+    End Sub
+End Class
