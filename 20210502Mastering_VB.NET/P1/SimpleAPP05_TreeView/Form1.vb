d1a8818eda718348a0287943511403b7410b79c2
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-05-09 15:20:38 +0800

    Add files via upload

---
+++ 20210502Mastering_VB.NET/P1/SimpleAPP05_TreeView/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With Me
+            .Label1.Text = "FORM1"
+        End With
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Form2.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim i As Integer
+        For i = 0 To 10
+            With Me
+                .ListBox1.Items.Add(i * i)
+                Threading.Thread.Sleep(500)
+            End With
+        Next
+
+        While (i > 0)
+            MessageBox.Show(i)
+            i = i - 1
+        End While
+    End Sub
+End Class
---
+++ 20210502Mastering_VB.NET/P1/SimpleAPP05_TreeView/Form2.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form2
+    Dim i As Integer = 0
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With Me
+            .Label1.Text = "FORM2"
+            .Button1.Text = "Add node"
+            .Text = "TreeView"
+        End With
+    End Sub
+
+    Private Sub Form2_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        Form1.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        i = (i + 1) Mod 4
+        Dim str As String
+        str = InputBox("Enter your node: ")
+        With Me
+            If .TreeView1.SelectedNode Is Nothing Then
+                .TreeView1.Nodes.Add(str, str, i, i)
+            Else
+                .TreeView1.SelectedNode.Nodes.Add(str, str, i, i)
+            End If
+        End With
+    End Sub
+End Class
