eb683861e88e471943aed66237c5ae13b2d96d4a
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-05-16 08:42:24 +0800

    Add files via upload

---
+++ 20210502Mastering_VB.NET/P1/SimpleAPP10_Var/Form1.Designer.vb
@@ -0,0 +1,428 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.GroupBox5 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox6 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.TextBox10 = New System.Windows.Forms.TextBox()
+        Me.GroupBox7 = New System.Windows.Forms.GroupBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.GroupBox8 = New System.Windows.Forms.GroupBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.GroupBox5.SuspendLayout()
+        Me.GroupBox6.SuspendLayout()
+        Me.GroupBox7.SuspendLayout()
+        Me.GroupBox8.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(27, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(96, 58)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(96, 96)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox2.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(230, 172)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Location = New System.Drawing.Point(248, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(180, 172)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(88, 85)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(66, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(16, 85)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(66, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(88, 48)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(66, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 48)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(66, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.TextBox3)
+        Me.GroupBox3.Location = New System.Drawing.Point(434, 12)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(186, 172)
+        Me.GroupBox3.TabIndex = 6
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "GroupBox3"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(41, 49)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 60)
+        Me.TextBox3.TabIndex = 7
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.TextBox6)
+        Me.GroupBox4.Controls.Add(Me.TextBox5)
+        Me.GroupBox4.Controls.Add(Me.TextBox4)
+        Me.GroupBox4.Location = New System.Drawing.Point(12, 204)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(608, 114)
+        Me.GroupBox4.TabIndex = 7
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "GroupBox4"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(463, 22)
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 75)
+        Me.TextBox6.TabIndex = 2
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(193, 22)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 75)
+        Me.TextBox5.TabIndex = 1
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(87, 22)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 75)
+        Me.TextBox4.TabIndex = 0
+        '
+        'GroupBox5
+        '
+        Me.GroupBox5.Controls.Add(Me.Button5)
+        Me.GroupBox5.Controls.Add(Me.TextBox9)
+        Me.GroupBox5.Controls.Add(Me.Label5)
+        Me.GroupBox5.Controls.Add(Me.TextBox8)
+        Me.GroupBox5.Controls.Add(Me.TextBox7)
+        Me.GroupBox5.Controls.Add(Me.Label4)
+        Me.GroupBox5.Controls.Add(Me.Label3)
+        Me.GroupBox5.Location = New System.Drawing.Point(12, 333)
+        Me.GroupBox5.Name = "GroupBox5"
+        Me.GroupBox5.Size = New System.Drawing.Size(608, 100)
+        Me.GroupBox5.TabIndex = 8
+        Me.GroupBox5.TabStop = False
+        Me.GroupBox5.Text = "GroupBox5"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(315, 36)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(463, 37)
+        Me.TextBox9.Multiline = True
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(100, 46)
+        Me.TextBox9.TabIndex = 10
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(463, 19)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 15)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Label5"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(87, 60)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(176, 23)
+        Me.TextBox8.TabIndex = 3
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(87, 29)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(176, 23)
+        Me.TextBox7.TabIndex = 2
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(17, 68)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 15)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Label4"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 37)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 15)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Label3"
+        '
+        'GroupBox6
+        '
+        Me.GroupBox6.Controls.Add(Me.ListBox1)
+        Me.GroupBox6.Location = New System.Drawing.Point(12, 455)
+        Me.GroupBox6.Name = "GroupBox6"
+        Me.GroupBox6.Size = New System.Drawing.Size(187, 152)
+        Me.GroupBox6.TabIndex = 9
+        Me.GroupBox6.TabStop = False
+        Me.GroupBox6.Text = "GroupBox6"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(6, 22)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(175, 124)
+        Me.ListBox1.TabIndex = 0
+        '
+        'TextBox10
+        '
+        Me.TextBox10.Location = New System.Drawing.Point(6, 22)
+        Me.TextBox10.Name = "TextBox10"
+        Me.TextBox10.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox10.TabIndex = 10
+        '
+        'GroupBox7
+        '
+        Me.GroupBox7.Controls.Add(Me.Button6)
+        Me.GroupBox7.Controls.Add(Me.ListBox2)
+        Me.GroupBox7.Controls.Add(Me.TextBox10)
+        Me.GroupBox7.Location = New System.Drawing.Point(205, 455)
+        Me.GroupBox7.Name = "GroupBox7"
+        Me.GroupBox7.Size = New System.Drawing.Size(197, 152)
+        Me.GroupBox7.TabIndex = 11
+        Me.GroupBox7.TabStop = False
+        Me.GroupBox7.Text = "GroupBox7"
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(116, 22)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 15
+        Me.ListBox2.Location = New System.Drawing.Point(6, 51)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(100, 94)
+        Me.ListBox2.TabIndex = 11
+        '
+        'GroupBox8
+        '
+        Me.GroupBox8.Controls.Add(Me.ListBox3)
+        Me.GroupBox8.Location = New System.Drawing.Point(408, 455)
+        Me.GroupBox8.Name = "GroupBox8"
+        Me.GroupBox8.Size = New System.Drawing.Size(200, 152)
+        Me.GroupBox8.TabIndex = 12
+        Me.GroupBox8.TabStop = False
+        Me.GroupBox8.Text = "GroupBox8"
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 15
+        Me.ListBox3.Location = New System.Drawing.Point(6, 22)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(188, 124)
+        Me.ListBox3.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(630, 626)
+        Me.Controls.Add(Me.GroupBox8)
+        Me.Controls.Add(Me.GroupBox7)
+        Me.Controls.Add(Me.GroupBox6)
+        Me.Controls.Add(Me.GroupBox5)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.GroupBox5.ResumeLayout(False)
+        Me.GroupBox5.PerformLayout()
+        Me.GroupBox6.ResumeLayout(False)
+        Me.GroupBox7.ResumeLayout(False)
+        Me.GroupBox7.PerformLayout()
+        Me.GroupBox8.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents GroupBox5 As GroupBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents GroupBox6 As GroupBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents GroupBox7 As GroupBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents GroupBox8 As GroupBox
+    Friend WithEvents ListBox3 As ListBox
+End Class
---
+++ 20210502Mastering_VB.NET/P1/SimpleAPP10_Var/Form1.vb
@@ -0,0 +1,211 @@
+﻿Structure Employee
+    Dim name As String
+    Dim age As Integer
+    Public Overrides Function ToString() As String
+        Return $"name: {name}, age: {age}"
+    End Function
+End Structure
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With Me
+            .Text = "Simple Application v0.01 @ZL, 20210515"
+            .Label1.Text = "A"
+            .Label2.Text = "B"
+
+            .TextBox1.Text = 100
+            .TextBox2.Text = 300
+
+            .Button1.Text = "+"
+            .Button1.AutoSize = True
+            .Button2.Text = "-"
+            .Button2.AutoSize = True
+            .Button3.Text = "*"
+            .Button3.AutoSize = True
+            .Button4.Text = "/"
+            .Button4.AutoSize = True
+
+            .GroupBox1.Text = "Operands"
+            .GroupBox2.Text = "Operators"
+            .GroupBox3.Text = "Result"
+            .GroupBox4.Text = "Analysis"
+
+            .Label3.Text = "CHN"
+            .Label4.Text = "JPN"
+            .Label5.Text = "Result"
+            .Button5.Text = "Evaluate"
+
+            .TextBox10.Text = 10
+            .Button6.Text = "calc"
+
+            ListCultureInfo(.ListBox1)
+            Colorize(.ListBox1)
+            Colorize(.TextBox9)
+        End With
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        With Me
+            Calculate(.TextBox1, .TextBox2, .TextBox3, .Button1.Text)
+            Colorize(.TextBox3)
+            Analyze()
+        End With
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        With Me
+            Calculate(.TextBox1, .TextBox2, .TextBox3, .Button2.Text)
+            Colorize(.TextBox3)
+            Analyze()
+        End With
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        With Me
+            Calculate(.TextBox1, .TextBox2, .TextBox3, .Button3.Text)
+            Colorize(.TextBox3)
+            Analyze()
+        End With
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        With Me
+            Calculate(.TextBox1, .TextBox2, .TextBox3, .Button4.Text)
+            Colorize(.TextBox3)
+            Analyze()
+        End With
+    End Sub
+
+    Private Shared Sub Calculate(control1 As Control, control2 As Control, control3 As Control, operand As String)
+        Dim a As Double, b As Double
+        Dim rv As Double
+
+        If Double.TryParse(control1.Text, a) And Double.TryParse(control2.Text, b) Then
+            Select Case operand
+                Case "+"
+                    rv = a + b
+                    control3.Text = rv.ToString()
+                Case "-"
+                    rv = a - b
+                    control3.Text = rv.ToString()
+                Case "*"
+                    rv = a * b
+                    control3.Text = rv.ToString()
+                Case "/"
+                    Try
+                        rv = a / b
+                        control3.Text = rv.ToString()
+                    Catch ex As DivideByZeroException
+                        control3.Text = "Inf"
+                    End Try
+            End Select
+        Else
+            MessageBox.Show("input error. can't parse")
+        End If
+    End Sub
+
+    Private Shared Sub Colorize(control1 As Control)
+        Dim r As Integer, g As Integer, b As Integer
+        'Dim a As Double
+        Dim rnd As New Random
+        r = rnd.Next(125, 255)
+        g = rnd.Next(150, 255)
+        b = rnd.Next(150, 255)
+        'a = rnd.NextDouble()
+        control1.BackColor = Color.FromArgb(r, g, b)
+    End Sub
+
+    Private Sub Analyze()
+        Dim input1 As String, input2 As String, input3 As String
+        With Me
+            input1 = .TextBox1.Text
+            input2 = .TextBox2.Text
+            input3 = .TextBox3.Text
+
+            If IsDigitals(input1) Then
+                .TextBox4.Text = TypeName(input1)
+            Else
+                .TextBox4.Text = vbNullString
+            End If
+
+            If IsDigitals(input2) Then
+                .TextBox5.Text = input2.GetType().ToString()
+            Else
+                .TextBox5.Text = vbNullString
+            End If
+
+            If IsDigitals(input3) Then
+                .TextBox6.Text = TypeName(input3)
+            Else
+                .TextBox6.Text = vbNullString
+            End If
+
+            Colorize(.TextBox4)
+            Colorize(.TextBox5)
+            Colorize(.TextBox6)
+        End With
+    End Sub
+
+    Private Shared Function IsDigitals(s As String) As Boolean
+        Dim valid As Boolean = True
+        Dim ch As Char
+        Dim i As Integer
+
+        For i = 0 To s.Length - 1
+            ch = s.Chars(i)
+            If Not (Char.IsDigit(ch) Or Char.IsSymbol(ch)) Then
+                valid = False
+                Exit For
+            End If
+        Next
+
+        Return valid
+    End Function
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim d1, d2 As Date
+        Dim CHN As New System.Globalization.CultureInfo("zh-Hans")
+        Dim JPN As New System.Globalization.CultureInfo("ja-JP")
+
+        d1 = DateTime.Parse("2021/05/15", CHN)
+        d2 = DateTime.Parse("2021/05/15", JPN)
+        With Me
+            .TextBox7.Text = d1.ToString()
+            .TextBox8.Text = d2.ToString()
+            .TextBox9.Text = d1.Equals(d2).ToString()
+        End With
+    End Sub
+
+    Private Shared Sub ListCultureInfo(control As ListBox)
+        For Each ci As System.Globalization.CultureInfo In System.Globalization.CultureInfo.GetCultures(System.Globalization.CultureTypes.AllCultures)
+            control.Items.Add(ci)
+        Next
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim sum As Integer = 0
+        Dim last As Integer
+        Me.ListBox2.Items.Clear()
+        If Integer.TryParse(Me.TextBox10.Text, last) Then
+            For i As Integer = 0 To last Step 2
+                sum += System.Math.Pow(i, 2)
+                Me.ListBox2.Items.Add(sum)
+            Next
+        End If
+
+        Dim emp1 As Employee, emp2 As Employee
+        emp1.name = "ZL"
+        emp1.age = 35
+
+        emp2.name = "TS"
+        emp2.age = 30
+
+        Dim emps As Employee() = {emp1, emp2}
+
+        For i As Integer = 0 To emps.Length - 1
+            With Me
+                .ListBox3.Items.Add(emps(i).ToString())
+            End With
+        Next
+
+    End Sub
+End Class
