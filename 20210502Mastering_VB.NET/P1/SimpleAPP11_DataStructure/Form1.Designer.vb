aead28e9fead0ec65803664a39b83c15cfffe9ed
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2021-05-17 07:32:44 +0800

    Add files via upload

---
+++ 20210502Mastering_VB.NET/P1/SimpleAPP11_DataStructure/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(12, 36)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(133, 259)
+        Me.ListBox1.TabIndex = 0
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 15
+        Me.ListBox2.Location = New System.Drawing.Point(151, 36)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(133, 259)
+        Me.ListBox2.TabIndex = 1
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 15
+        Me.ListBox3.Location = New System.Drawing.Point(290, 36)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(133, 259)
+        Me.ListBox3.TabIndex = 3
+        '
+        'ListBox4
+        '
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.ItemHeight = 15
+        Me.ListBox4.Location = New System.Drawing.Point(429, 36)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(133, 259)
+        Me.ListBox4.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 15)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(151, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(290, 18)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(429, 18)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 15)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Label4"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 301)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(151, 301)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(290, 301)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(429, 301)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(583, 375)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ 20210502Mastering_VB.NET/P1/SimpleAPP11_DataStructure/Form1.vb
@@ -0,0 +1,119 @@
+﻿Structure Employee
+    Dim id As Integer
+    Dim name As String
+    Dim age As Integer
+    Public Sub New(id, name, age)
+        Me.id = id
+        Me.name = name
+        Me.age = age
+    End Sub
+    Public Overrides Function ToString() As String
+        Return $"id: {Me.id}, name: {Me.name}, age: {Me.age}"
+    End Function
+End Structure
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With Me
+            .Text = "Simple Application, v0.01 @ZL, 20210516"
+
+            .Label1.Text = "array"
+            .Label2.Text = "list"
+            .Label3.Text = "dict"
+            .Label4.Text = "set"
+
+            .Button1.Text = "populate array"
+            .Button1.AutoSize = True
+            .Button2.Text = "populate list"
+            .Button2.AutoSize = True
+            .Button3.Text = "populate dict"
+            .Button3.AutoSize = True
+            .Button4.Text = "populate set"
+            .Button4.AutoSize = True
+
+        End With
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim emps(10) As Employee
+        emps(0) = New Employee(1, "ZL", 35)
+        emps(1) = New Employee(2, "TS", 30)
+        emps(2) = New Employee(3, "SCY", 35)
+        emps(3) = New Employee(1, "ZL", 35)
+        emps(4) = New Employee(2, "TS", 30)
+        emps(5) = New Employee(3, "SCY", 35)
+        emps(6) = New Employee(1, "ZL", 35)
+        emps(7) = New Employee(2, "TS", 30)
+        emps(8) = New Employee(3, "SCY", 35)
+
+        For Each emp As Employee In emps
+            With Me
+                .ListBox1.Items.Add(emp.ToString())
+            End With
+        Next
+        Colorize(Me.ListBox1)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim emps As New List(Of Employee)
+        emps.Add(New Employee(1, "ZL", 35))
+        emps.Add(New Employee(2, "TS", 30))
+        emps.Add(New Employee(3, "SCY", 35))
+        emps.Add(New Employee(1, "ZL", 35))
+        emps.Add(New Employee(2, "TS", 30))
+        emps.Add(New Employee(3, "SCY", 35))
+        emps.Add(New Employee(1, "ZL", 35))
+        emps.Add(New Employee(2, "TS", 30))
+        emps.Add(New Employee(3, "SCY", 35))
+
+        With Me
+            For Each emp As Employee In emps
+                .ListBox2.Items.Add(emp.ToString())
+            Next
+        End With
+        Colorize(Me.ListBox2)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim emps As New Dictionary(Of Integer, Employee)
+        emps.Add(0, New Employee(1, "ZL", 35))
+        emps.Add(1, New Employee(2, "TS", 30))
+        emps.Add(2, New Employee(3, "SCY", 35))
+
+        With Me
+            For Each kv As KeyValuePair(Of Integer, Employee) In emps
+                .ListBox3.Items.Add(kv.Value.ToString())
+            Next
+        End With
+        Colorize(Me.ListBox3)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim emps As New HashSet(Of Employee) From {
+            New Employee(1, "ZL", 35),
+            New Employee(2, "TS", 30),
+            New Employee(3, "SCY", 35),
+            New Employee(1, "ZL", 35),
+            New Employee(2, "TS", 30),
+            New Employee(3, "SCY", 35)
+        }
+
+        With Me
+            For Each emp As Employee In emps
+                .ListBox4.Items.Add(emp.ToString())
+            Next
+        End With
+        Colorize(Me.ListBox4)
+    End Sub
+
+    Private Shared Sub Colorize(control As ListBox)
+        Dim rnd As New Random
+        Dim R As Integer, G As Integer, B As Integer
+        Dim lbound As Integer = 150
+        Dim ubound As Integer = 255
+        R = rnd.Next(lbound, ubound)
+        G = rnd.Next(lbound, ubound)
+        B = rnd.Next(lbound, ubound)
+        control.BackColor = Color.FromArgb(R, G, B)
+    End Sub
+End Class
